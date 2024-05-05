1e834a335905bee5e28a5d4794264d58348ae975
B0hdan2 <118682191+B0hdan2@users.noreply.github.com>
2024-05-05 14:23:33 +0300

    homework React-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SipHappensCafe from "./SipHappensCafe/SipHappensCafe.jsx";
 import Options from "./Options/Options.jsx";
 import Feedback from "./Feedback/Feedback.jsx";
 
 const App = () => {
   const [reviews, setReviews] = useState(() => {
+    const savedClicks = JSON.parse(window.localStorage.getItem('feedback'))
+
+    if (savedClicks !== null) {
+      return savedClicks;
+    }
+    
     return {
       good: 0,
       neutral: 0,
       bad: 0,
     };
   });
+
+useEffect(()=>{
+window.localStorage.setItem('feedback', JSON.stringify({...reviews}))
+},[reviews])
 
   const updateFeedback = (feedbackType) => {
     if (feedbackType === "reset") {
